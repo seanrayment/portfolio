d6ae9a20cf8e4da77ff953890a7524515c3ea972
seanrayment <rayment.code@gmail.com>
2016-10-09 16:34:26 -0400

    finished portfolio

--- main.js
+++ main.js
@@ -22,13 +22,41 @@ $(document).ready(function() {
 	$("#tab-one").click(function() {
 		$("#tab1").css("display","flex");
 		$("#tab2").css("display","none");
+		$("#tab3").css("display","none");
+		$("#tab4").css("display","none");
 		$("#tab-one").css("backgroundColor", "#3949ab");
 		$("#tab-two").css("backgroundColor", "#7986cb");
+		$("#tab-three").css("backgroundColor", "#7986cb");
+		$("#tab-four").css("backgroundColor", "#7986cb");
 	})
 	$("#tab-two").click(function() {
-		$("#tab2").css("display","flex");
 		$("#tab1").css("display","none");
-		$("#tab-two").css("backgroundColor", "#3949ab");
+		$("#tab2").css("display","flex");
+		$("#tab3").css("display","none");
+		$("#tab4").css("display","none");
 		$("#tab-one").css("backgroundColor", "#7986cb");
+		$("#tab-two").css("backgroundColor", "#3949ab");
+		$("#tab-three").css("backgroundColor", "#7986cb");
+		$("#tab-four").css("backgroundColor", "#7986cb");
 	})
+	$("#tab-three").click(function() {
+		$("#tab1").css("display","none");
+		$("#tab2").css("display","none");
+		$("#tab3").css("display","flex");
+		$("#tab4").css("display","none");
+		$("#tab-one").css("backgroundColor", "#7986cb");
+		$("#tab-two").css("backgroundColor", "#7986cb");
+		$("#tab-three").css("backgroundColor", "#3949ab");
+		$("#tab-four").css("backgroundColor", "#7986cb");
 	})
+	$("#tab-four").click(function() {
+		$("#tab1").css("display","none");
+		$("#tab2").css("display","none");
+		$("#tab3").css("display","none");
+		$("#tab4").css("display","flex");
+		$("#tab-one").css("backgroundColor", "#7986cb");
+		$("#tab-two").css("backgroundColor", "#7986cb");
+		$("#tab-three").css("backgroundColor", "#7986cb");
+		$("#tab-four").css("backgroundColor", "#3949ab");
+	})
+})
