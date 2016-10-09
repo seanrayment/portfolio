dd1fb066d54308991af45ae46db8433492d381ab
seanrayment <rayment.code@gmail.com>
2016-10-09 15:57:20 -0400

    made changes to the projects section

--- main.js
+++ main.js
@@ -14,7 +14,21 @@ $(document).ready(function() {
 		$("#profile_text").css("display","none");
 		$("#profile").animate(
 			{borderRadius: "100%"},
-			200);
+			1000);
 	})
 })
 
+$(document).ready(function() {
+	$("#tab-one").click(function() {
+		$("#tab1").css("display","flex");
+		$("#tab2").css("display","none");
+		$("#tab-one").css("backgroundColor", "#3949ab");
+		$("#tab-two").css("backgroundColor", "#7986cb");
+	})
+	$("#tab-two").click(function() {
+		$("#tab2").css("display","flex");
+		$("#tab1").css("display","none");
+		$("#tab-two").css("backgroundColor", "#3949ab");
+		$("#tab-one").css("backgroundColor", "#7986cb");
+	})
+	})
